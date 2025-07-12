e357415e683c6ee6ccae6f721300c38391b92066
Anusathya1991 <anuviswa2306@gmail.com>
2025-07-12 09:34:20 +0530

    Add files via upload

---
+++ Assignment1/InheritanceAssignment2/checkboxButton.java
@@ -0,0 +1,9 @@
+package week3.InheritanceAssignment2;
+
+public class checkboxButton extends Buttonsubclass1
+{
+	
+	  public void clickCheckButton() {
+	        System.out.println("Checkbox button clicked----->Subclass2");
+	    }
+}
